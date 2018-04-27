c26d101dd8f5e950a0a374443a22de8f82449c08
muther <mutheraks83@gmail.com>
2018-04-27 11:50:24 +0300

    first program
    this is my first program

---
+++ first project/first project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is my first program
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
