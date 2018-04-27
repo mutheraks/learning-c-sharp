687942b7ad4a38a3c69af09aba54c666770907cf
muther <mutheraks83@gmail.com>
2018-04-27 11:56:04 +0300

    this is first variable

---
+++ variable/variable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int my_var = 7;
+            Console.WriteLine("this is my value ={0}", my_var);
+            Console.ReadKey();
+        }
+    }
+}
